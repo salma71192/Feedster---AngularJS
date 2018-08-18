b0958f7ba6b30920e5bda03c2255b7c4a25617fc
Salma Mohammed <sm.71175@gmail.com>
2018-08-18 13:34:46 +0200

    refactor code

--- js/directives/feedsterPost.js
+++ js/directives/feedsterPost.js
@@ -1,9 +1,9 @@
-app.directive('feedsterPost', function() { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-      post: '=' 
-    }, 
-    templateUrl: 'js/directives/feedsterPost.html' 
+app.directive("feedsterPost", function() {
+  return {
+    restrict: 'E',
+    scope: {
+      post: '='
+    },
+    templateUrl: 'js/directives/feedsterPost.html'
   }
 });
